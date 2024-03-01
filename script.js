4138ddffd416b0904b1775d0b126c42e2846c544
GrantBeaton <159183393+GrantBeaton@users.noreply.github.com>
2024-03-01 14:30:01 -0500

    Friday Mar 2 Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function greeter() {
+    let name = prompt("What's your name?");
+    document.getElementById("greet").innerHTML =
+    "Hi "+name+"!";
+  }
+  
+  function food() {
+    let fav = prompt("What's your favourite food?");
+    document.getElementById("fav").innerHTML =
+    "My favourite food is also "+fav+"!";
+  }
+
+  function house() {
+    let house = prompt("What's your address?");
+    document.getElementById("house").innerHTML =
+    "Expect a delivery of your favourite food at "+house+"!";
+  }
